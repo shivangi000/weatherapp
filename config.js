87cac615d8e6688473cb996be7908bcb6b20c5d8
shivangi000 <87198804+shivangi000@users.noreply.github.com>
2022-03-21 21:54:28 +0530

    commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "846304cd4c22cd5e5a0b198a775325a7"
+    }
+}
+
+module.exports = constants;
